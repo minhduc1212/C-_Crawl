6c0925d047f40d8a998033852507157f747f544a
minhduc1212 <minhduc6e12@gmail.com>
2026-02-16 12:03:55 +0700

    simple crawl

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Text;
+
+HttpClient client = new HttpClient();
+string url = "https://www.doctruyenvoz.com/2014/09/7-ngay-lam-gia-su.html";
+try
+{
+    HttpResponseMessage response =  await client.GetAsync(url);
+    response.EnsureSuccessStatusCode();
+
+    string responseContent = await response.Content.ReadAsStringAsync();
+    
+    var htmlDoc = new HtmlDocument();
+    htmlDoc.LoadHtml(responseContent);
+
+    var divContent = htmlDoc.DocumentNode.SelectNodes("//div[@class='post-body-inner']");
+
+    
+    
+    string content =  divContent[0].InnerText;
+    
+    string filePath = "content.txt";
+    File.WriteAllText(filePath, content);   
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine(e.Message);
+}
--- obj/Debug/net9.0/Crawl with c#.AssemblyInfo.cs
+++ obj/Debug/net9.0/Crawl with c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crawl with c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9fb679fcab71e43ddb10445cc5c5460d4faadd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crawl with c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crawl with c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
